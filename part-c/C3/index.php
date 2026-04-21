de4ac34c43a29c3cf3093668225c401cee589602
yukkuri <reityan.sakura0104@gmail.com>
2026-04-21 21:50:28 +0900

    C3

--- part-c/C3/index.php
+++ part-c/C3/index.php
@@ -1,4 +1,40 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+  $uploadDir = "uploads/";
+
+  if (!is_dir($uploadDir)) {
+    mkdir($uploadDir, 0755, true);
+  };
+
+
+  $file = $_FILES["file"];
+  if ($file["error"] !== UPLOAD_ERR_OK) {
+    die("アップロードエラー" . $file["error"]);
+  };
+
+  if ($file["size"] > 2 * 1024 * 1024) {
+    die("ファイルサイズが大きすぎます");
+  };
+
+  $AllowExt = ["jpeg", "png", "gif"];
+  $fileExt = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+
+  if (!in_array($fileExt, $AllowExt)) {
+    die("ファイル形式が不正です");
+  };
+
+  $newFilename = uniqid() . "." . $fileExt;
+  $location = $uploadDir . $newFilename;
+
+  if (!move_uploaded_file($file["tmp_name"], $location)) {
+    die("保存失敗");
+  };
+  echo "ファイルがアップロードされました。<br>";
+  echo "ファイル名:" . htmlspecialchars($newFilename) . "<br>";
+  echo "ファイルサイズ:" . $file["size"] . "byte<br>";
+  echo "MIMEタイプ:" . $file["type"] . "<br>";
+};
 
 
 
@@ -6,13 +42,20 @@
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>C3</title>
   <link rel="stylesheet" href="style.css">
 </head>
-<body>
 
+<body>
+  <form method="POST" enctype="multipart/form-data">
+    <label for="file">ファイルを選択:</label>
+    <input type="file" name="file" id="file">
+    <button type="submit">送信</button>
+  </form>
 </body>
+
 </html>
